0454cc0e0eedf7c3fe74eabd1186e90d01c90dd3
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-04-10 17:53:18 +0000

    Add project files.

---
+++ ST10369554_POE_Part1/Ingredient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10369554_POE_Part1
+{
+    internal class Ingredient
+    {
+        //Declaring variables
+        public string[] Names { get; set; }
+        public double[] Quantities { get; set; }
+        public string[] Units { get; set; }
+
+        public Ingredient(int size)
+        {
+            Names = new string[size];
+            Quantities = new double[size];
+            Units = new string[size];
+        }
+        public override string ToString()
+        {
+            string[]
+        }
+    }
+}
---
+++ ST10369554_POE_Part1/Step.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10369554_POE_Part1
+{
+    internal class Step
+    {
+    }
+}
