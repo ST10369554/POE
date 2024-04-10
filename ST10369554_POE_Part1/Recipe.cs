0fa9cea975360bf22c86c306262a071cdc058883
Sendzo Matooane <ST10369554@rcconnect.edu.za>
2024-04-10 19:32:43 +0000

    Declaring variables for Recipe class

--- ST10369554_POE_Part1/Recipe.cs
+++ ST10369554_POE_Part1/Recipe.cs
@@ -8,5 +8,11 @@ namespace ST10369554_POE_Part1
 {
     internal class Recipe
     {
+        public string[] IngredName { get; set; }
+        public double[] IngredQuantity { get; set; }
+        public string[] IngredUnit { get; set; }
+        public Step[] steps { get; set; }
+
+        
     }
 }
